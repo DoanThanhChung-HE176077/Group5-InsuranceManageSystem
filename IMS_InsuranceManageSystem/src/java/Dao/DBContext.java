cf5762758a3d468972cc65db1c7fcee0700b88fa
chunChu <86117554+ChungDoan@users.noreply.github.com>
2023-09-20 00:09:19 +0700

    add db context

---
+++ IMS_InsuranceManageSystem/src/java/Dao/DBContext.java
@@ -0,0 +1,46 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FPT University - PRJ301
+ */
+public class DBContext {
+    protected Connection connection;
+    protected PreparedStatement statement;
+    protected ResultSet resultSet;
+    
+    public DBContext()
+    {
+        //@Students: You are allowed to edit user, pass, url variables to fit 
+        //your system configuration
+        //You can also add more methods for Database Interaction tasks. 
+        //But we recommend you to do it in another class
+        // For example : StudentDBContext extends DBContext , 
+        //where StudentDBContext is located in dal package, 
+        try {
+            //enter pass and acc sql
+            String user = "sa1";
+            String pass = "123";
+            
+            //edit server name, database name
+            String url = "jdbc:sqlserver://DESKTOP-7MF95U3:1433;databaseName=CourseManagementSystem";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
