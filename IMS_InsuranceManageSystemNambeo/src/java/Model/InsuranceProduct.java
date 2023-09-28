662c956f4b4345e41f9e123182aa26afbb86decb
TuanAnhh-K17HL <anhnpthe170999@fpt.edu.vn>
2023-09-28 08:34:11 +0700

    uiyhui

---
+++ IMS_InsuranceManageSystemNambeo/src/java/Model/InsuranceProduct.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+
+
+/**
+ *
+ * @author ADMIN
+ */
+public class InsuranceProduct {
+    private int ip_id;
+    private String ip_type;
+    private String ip_name;
+    
+    public InsuranceProduct(){
+       
+    }
+    public InsuranceProduct(int ip_id,String ip_type,String ip_name){
+        this.ip_id= ip_id;
+        this.ip_type = ip_type;
+        this.ip_name = ip_name;
+         
+    }
+    
+    public int getIp_id() {
+        return ip_id;
+    }
+
+    public String getIp_type() {
+        return ip_type;
+    }
+
+    public String getIp_name() {
+        return ip_name;
+    }
+
+
+    public void setIp_id(int ip_id) {
+        this.ip_id = ip_id;
+    }
+
+    public void setIp_type(String ip_type) {
+        this.ip_type = ip_type;
+    }
+
+    public void setIp_name(String ip_name) {
+        this.ip_name = ip_name;
+    }
+
+    @Override
+    public String toString() {
+        return "InsuranceProduct{" + "ip_id=" + ip_id + ", ip_type=" + ip_type + ", ip_name=" + ip_name + '}';
+    }
+ 
+    
+    
+    
+    
+    
+  
+   
+
+}
+
+
+
