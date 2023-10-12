44043e1e752a7def443fa8d614fb78794d0b1d8f
chunChu <86117554+DoanTChung-HE176077@users.noreply.github.com>
2023-10-12 10:38:16 +0700

    blog update

--- IMS_InsuranceManageSystem/src/java/Dao/BlogDAO.java
+++ IMS_InsuranceManageSystem/src/java/Dao/BlogDAO.java
@@ -29,13 +29,16 @@ public class BlogDAO extends DBContext {
                 String bl_id = rs.getString(1);
                 String bl_title = rs.getString(2);
                 String bl_content = rs.getString(3);
-                String bl_like = rs.getString(4);
+                int bl_like = rs.getInt(4);
                 String bl_img = rs.getString(5);
-                String bl_author = rs.getString(6);
+                int user_id = rs.getInt(6);
                 String bl_creationdate = rs.getString(7);
-                int bl_view = rs.getInt(8);
-                String bl_status = rs.getString(9);
-                getAll.add(new Blogs(bl_id, bl_title, bl_content, bl_like, bl_img, bl_author, bl_creationdate, bl_view, bl_view, bl_status));
+                int bl_type_id = rs.getInt(8);
+                int bl_tag_id = rs.getInt(9);
+                int bl_view = rs.getInt(10);
+                String bl_status = rs.getString(11);
+
+                getAll.add(new Blogs(bl_id, bl_title, bl_content, bl_like, bl_img, user_id, bl_creationdate, bl_type_id, bl_tag_id, bl_view, bl_status));
             }
             return getAll;
         } catch (SQLException ex) {
@@ -55,14 +58,17 @@ public class BlogDAO extends DBContext {
                 String bl_id = rs.getString(1);
                 String bl_title = rs.getString(2);
                 String bl_content = rs.getString(3);
-                String bl_like = rs.getString(4);
+                int bl_like = rs.getInt(4);
                 String bl_img = rs.getString(5);
-                String bl_author = rs.getString(6);
+                int user_id = rs.getInt(6);
                 String bl_creationdate = rs.getString(7);
-                int bl_tag_id = rs.getInt(8);
-                int bl_view = rs.getInt(9);
-                String bl_status = rs.getString(10);
-                getTop5Blogs.add(new Blogs(bl_id, bl_title, bl_content, bl_like, bl_img, bl_author, bl_creationdate, bl_tag_id, bl_view, bl_status));
+                int bl_type_id = rs.getInt(8);
+                int bl_tag_id = rs.getInt(9);
+                int bl_view = rs.getInt(10);
+                String bl_status = rs.getString(11);
+                
+                getTop5Blogs.add(new Blogs(bl_id, bl_title, bl_content, bl_like, bl_img, user_id, bl_creationdate, bl_type_id, bl_tag_id, bl_view, bl_status));
+
             }
             return getTop5Blogs;
         } catch (SQLException ex) {
@@ -82,14 +88,17 @@ public class BlogDAO extends DBContext {
                 String bl_id = rs.getString(1);
                 String bl_title = rs.getString(2);
                 String bl_content = rs.getString(3);
-                String bl_like = rs.getString(4);
+                int bl_like = rs.getInt(4);
                 String bl_img = rs.getString(5);
-                String bl_author = rs.getString(6);
+                int user_id = rs.getInt(6);
                 String bl_creationdate = rs.getString(7);
-                int bl_tag_id = rs.getInt(8);
-                int bl_view = rs.getInt(9);
-                String bl_status = rs.getString(10);
-                getTop15Blogs.add(new Blogs(bl_id, bl_title, bl_content, bl_like, bl_img, bl_author, bl_creationdate, bl_tag_id, bl_view, bl_status));
+                int bl_type_id = rs.getInt(8);
+                int bl_tag_id = rs.getInt(9);
+                int bl_view = rs.getInt(10);
+                String bl_status = rs.getString(11);
+
+                getTop15Blogs.add(new Blogs(bl_id, bl_title, bl_content, bl_like, bl_img, user_id, bl_creationdate, bl_type_id, bl_tag_id, bl_view, bl_status));
+
             }
             return getTop15Blogs;
         } catch (SQLException ex) {
@@ -98,53 +107,87 @@ public class BlogDAO extends DBContext {
         return null;
     }
 
-    public void addBlog(Blogs blog) {
+//    public void addBlog(Blogs blog) {
+//        try {
+//            String sql = "INSERT INTO [dbo].[Blogs]\n"
+//                    + "           ([bl_title]\n"
+//                    + "           ,[bl_content]\n"
+//                    + "           ,[bl_like]\n"
+//                    + "           ,[bl_img]\n"
+//                    + "           ,[bl_author]\n"
+//                    + "           ,[bl_creationdate]\n"
+//                    + "           ,[bl_tag_id]\n"
+//                    + "           ,[bl_view]\n"
+//                    + "           ,[bl_status])\n"
+//                    + "     VALUES\n"
+//                    + "           (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//            PreparedStatement ps = connection.prepareStatement(sql);
+//            ps.setString(1, blog.getBl_title());
+//            ps.setString(2, blog.getBl_content());
+//            ps.setString(3, blog.getBl_like());
+//            ps.setString(4, blog.getBl_img());
+//            ps.setString(5, blog.getBl_author());
+//            ps.setString(6, blog.getBl_creationdate());
+//            ps.setInt(7, blog.getBl_tag_id());
+//            ps.setInt(8, blog.getBl_view());
+//            ps.setString(9, blog.getBl_status());
+//        } catch (SQLException e) {
+//            Logger.getLogger(BlogDAO.class.getName()).log(Level.SEVERE, e.getMessage());
+//        }
+//    }
+    
+    //create new blog
+     public boolean createBlog(
+            String bl_title,
+            String bl_content,
+            int bl_like,
+            String bl_img,
+            int user_id,
+            String bl_creationdate,
+            int bl_type_id,
+            int bl_tag_id,
+            int bl_view,
+            String bl_status) {
         try {
-            String sql = "INSERT INTO [dbo].[Blogs]\n"
-                    + "           ([bl_title]\n"
-                    + "           ,[bl_content]\n"
-                    + "           ,[bl_like]\n"
-                    + "           ,[bl_img]\n"
-                    + "           ,[bl_author]\n"
-                    + "           ,[bl_creationdate]\n"
-                    + "           ,[bl_tag_id]\n"
-                    + "           ,[bl_view]\n"
-                    + "           ,[bl_status])\n"
-                    + "     VALUES\n"
-                    + "           (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, blog.getBl_title());
-            ps.setString(2, blog.getBl_content());
-            ps.setString(3, blog.getBl_like());
-            ps.setString(4, blog.getBl_img());
-            ps.setString(5, blog.getBl_author());
-            ps.setString(6, blog.getBl_creationdate());
-            ps.setInt(7, blog.getBl_tag_id());
-            ps.setInt(8, blog.getBl_view());
-            ps.setString(9, blog.getBl_status());
-        } catch (SQLException e) {
-            Logger.getLogger(BlogDAO.class.getName()).log(Level.SEVERE, e.getMessage());
+            String sql = "insert into Blogs values((SELECT COALESCE(MAX(bl_id), 0) from Blogs)+1,?,?,?,?,?,?,?,?,?,?) ;";
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setString(1, bl_title);
+            pstm.setString(2, bl_content);
+            pstm.setInt(3, bl_like);
+            pstm.setString(4, bl_img);
+            pstm.setInt(5, user_id);
+            pstm.setString(6, bl_creationdate);
+            pstm.setInt(7, bl_type_id);
+            pstm.setInt(8, bl_tag_id);
+            pstm.setInt(9, bl_view);
+            pstm.setString(10, bl_status);
+            
+            pstm.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(BlogDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return false;
     }
 
-    public int getLastId() {
-        int lastId;
-        try {
-            String strSQL = "SELECT top 1 bl_id\n"
-                    + "  FROM [dbo].[Blogs]\n"
-                    + "  ORDER BY bl_id DESC";
-            PreparedStatement pstm = connection.prepareStatement(strSQL);
-            ResultSet rs = pstm.executeQuery();
-            if (rs.next()) {
-                // Add thành công
-                lastId = rs.getInt(1);
-                return lastId;
-            }
-        } catch (SQLException e) {
-            Logger.getLogger(BlogDAO.class.getName()).log(Level.SEVERE, e.getMessage());
-        }
-        return 0;
-    }
+//    public int getLastId() {
+//        int lastId;
+//        try {
+//            String strSQL = "SELECT top 1 bl_id\n"
+//                    + "  FROM [dbo].[Blogs]\n"
+//                    + "  ORDER BY bl_id DESC";
+//            PreparedStatement pstm = connection.prepareStatement(strSQL);
+//            ResultSet rs = pstm.executeQuery();
+//            if (rs.next()) {
+//                // Add thành công
+//                lastId = rs.getInt(1);
+//                return lastId;
+//            }
+//        } catch (SQLException e) {
+//            Logger.getLogger(BlogDAO.class.getName()).log(Level.SEVERE, e.getMessage());
+//        }
+//        return 0;
+//    }
 
     public static void main(String[] args) {
         BlogDAO dao = new BlogDAO();
@@ -154,10 +197,11 @@ public class BlogDAO extends DBContext {
 //        for (Blogs blogs : bg) {
 //            System.out.println(blogs.toString());
 //        }
-        ArrayList<Blogs> bg = dao.getTop5Blogs();
-        for (Blogs blogs : bg) {
-            System.out.println(blogs.toString());
-        }
+//        ArrayList<Blogs> bg = dao.getTop5Blogs();
+//        for (Blogs blogs : bg) {
+//            System.out.println(blogs.toString());
+//        }
+        dao.createBlog("a", "b", 0, "bl_img", 1, "2023-09-01", 1, 1, 0, "Active");
     }
 
 }
--- IMS_InsuranceManageSystem/src/java/Model/Blogs.java
+++ IMS_InsuranceManageSystem/src/java/Model/Blogs.java
@@ -13,10 +13,11 @@ public class Blogs {
     String bl_id;
     String bl_title;
     String bl_content;
-    String bl_like;
+    int bl_like;
     String bl_img;
-    String bl_author;
+    int user_id;
     String bl_creationdate;
+    int bl_type_id ;
     int bl_tag_id;
     int bl_view;
     String bl_status;
@@ -24,67 +25,18 @@ public class Blogs {
     public Blogs() {
     }
 
-    public Blogs(String bl_id, String bl_title, String bl_content, String bl_like, String bl_img, String bl_author, String bl_creationdate, int bl_tag_id, int bl_view, String bl_status) {
+    public Blogs(String bl_id, String bl_title, String bl_content, int bl_like, String bl_img, int user_id, String bl_creationdate, int bl_type_id, int bl_tag_id, int bl_view, String bl_status) {
         this.bl_id = bl_id;
         this.bl_title = bl_title;
         this.bl_content = bl_content;
         this.bl_like = bl_like;
         this.bl_img = bl_img;
-        this.bl_author = bl_author;
+        this.user_id = user_id;
         this.bl_creationdate = bl_creationdate;
+        this.bl_type_id = bl_type_id;
         this.bl_tag_id = bl_tag_id;
         this.bl_view = bl_view;
         this.bl_status = bl_status;
-    }
-
-    public int getBl_tag_id() {
-        return bl_tag_id;
-    }
-
-    public void setBl_tag_id(int bl_tag_id) {
-        this.bl_tag_id = bl_tag_id;
-    }
-
-
-
-    public int getBl_view() {
-        return bl_view;
-    }
-
-    public void setBl_view(int bl_view) {
-        this.bl_view = bl_view;
-    }
-
-    public String getBl_id() {
-        return bl_id;
-    }
-
-    public String getBl_title() {
-        return bl_title;
-    }
-
-    public String getBl_content() {
-        return bl_content;
-    }
-
-    public String getBl_like() {
-        return bl_like;
-    }
-
-    public String getBl_img() {
-        return bl_img;
-    }
-
-    public String getBl_author() {
-        return bl_author;
-    }
-
-    public String getBl_creationdate() {
-        return bl_creationdate;
-    }
-
-    public String getBl_status() {
-        return bl_status;
     }
 
     public void setBl_id(String bl_id) {
@@ -99,7 +51,7 @@ public class Blogs {
         this.bl_content = bl_content;
     }
 
-    public void setBl_like(String bl_like) {
+    public void setBl_like(int bl_like) {
         this.bl_like = bl_like;
     }
 
@@ -107,48 +59,84 @@ public class Blogs {
         this.bl_img = bl_img;
     }
 
-    public void setBl_author(String bl_author) {
-        this.bl_author = bl_author;
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
     }
 
     public void setBl_creationdate(String bl_creationdate) {
         this.bl_creationdate = bl_creationdate;
     }
 
+    public void setBl_type_id(int bl_type_id) {
+        this.bl_type_id = bl_type_id;
+    }
+
+    public void setBl_tag_id(int bl_tag_id) {
+        this.bl_tag_id = bl_tag_id;
+    }
+
+    public void setBl_view(int bl_view) {
+        this.bl_view = bl_view;
+    }
+
     public void setBl_status(String bl_status) {
         this.bl_status = bl_status;
     }
 
+    public String getBl_id() {
+        return bl_id;
+    }
+
+    public String getBl_title() {
+        return bl_title;
+    }
+
+    public String getBl_content() {
+        return bl_content;
+    }
+
+    public int getBl_like() {
+        return bl_like;
+    }
+
+    public String getBl_img() {
+        return bl_img;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public String getBl_creationdate() {
+        return bl_creationdate;
+    }
+
+    public int getBl_type_id() {
+        return bl_type_id;
+    }
+
+    public int getBl_tag_id() {
+        return bl_tag_id;
+    }
+
+    public int getBl_view() {
+        return bl_view;
+    }
+
+    public String getBl_status() {
+        return bl_status;
+    }
+
     @Override
     public String toString() {
-        String tableFormat = String.format(
-                "Blogs{\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "| %-12s | %s\n"
-                + "}",
-                "bl_id", bl_id,
-                "bl_title", bl_title,
-                "bl_content", bl_content,
-                "bl_like", bl_like,
-                "bl_img", bl_img,
-                "bl_author", bl_author,
-                "bl_creationdate", bl_creationdate,
-                "bl_tag_id", bl_tag_id,
-                "bl_view", bl_view,
-                "bl_status", bl_status
-        );
-
-        return tableFormat;
-
+        return "Blogs{" + "bl_id=" + bl_id + ", bl_title=" + bl_title + ", bl_content=" + bl_content + ", bl_like=" + bl_like + ", bl_img=" + bl_img + ", user_id=" + user_id + ", bl_creationdate=" + bl_creationdate + ", bl_type_id=" + bl_type_id + ", bl_tag_id=" + bl_tag_id + ", bl_view=" + bl_view + ", bl_status=" + bl_status + '}';
     }
+    
+    
+
+   
+    
+
 
 
 
