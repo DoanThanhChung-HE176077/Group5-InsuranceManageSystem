950441a7e3db973fbc12f4459348658a1b05cedd
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2023-10-14 22:43:04 +0700

    newBl syntax fix

--- IMS_InsuranceManageSystem/src/java/Model/newBl.java
+++ IMS_InsuranceManageSystem/src/java/Model/newBl.java
@@ -8,13 +8,13 @@ package Model;
  *
  * @author chun
  */
-public class NewBl extends Blogs{
+public class newBl extends Blogs{
     String usernameString;
 
-    public NewBl() {
+    public newBl() {
     }
 
-    public NewBl(String usernameString, String bl_id, String bl_title, String bl_content, int bl_like, String bl_img, int user_id, String bl_creationdate, String bl_type_name, String bl_tag_tagname, int bl_view, String bl_status) {
+    public newBl(String usernameString, String bl_id, String bl_title, String bl_content, int bl_like, String bl_img, int user_id, String bl_creationdate, String bl_type_name, String bl_tag_tagname, int bl_view, String bl_status) {
         super(bl_id, bl_title, bl_content, bl_like, bl_img, user_id, bl_creationdate, bl_type_name, bl_tag_tagname, bl_view, bl_status);
         this.usernameString = usernameString;
     }
