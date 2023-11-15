bb2c21272d3ff4f99c1dda910842b6919652cfa6
DoDucThien-HE176216 <thienduc2552003@gmail.com>
2023-11-15 13:51:55 +0700

    update

--- IMS_InsuranceManageSystem/src/java/dao/DBContext.java
+++ IMS_InsuranceManageSystem/src/java/dao/DBContext.java
@@ -64,9 +64,9 @@ public class DBContext {
                                       >>>>>>>>>>>>>          <<<<<<<<<<<<
                                       ======= Edit SQL config here ========*/
             
-//            String user = "sa";
-//             String pass = "123";
-//            String url = "jdbc:sqlserver://localhost:1433;databaseName=insurance-manage-systemV8";
+            String user = "sa";
+             String pass = "123";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=insurance-manage-systemV8";
 
 
 
@@ -89,9 +89,9 @@ public class DBContext {
                                       >>>>>>>>>>>>>          <<<<<<<<<<<<
                                       ======= Edit SQL config here ========*/
             
-            String user = "sa";
-            String pass = "1234";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=insurance-manage-systemV6";
+//            String user = "sa";
+//            String pass = "1234";
+//            String url = "jdbc:sqlserver://localhost:1433;databaseName=insurance-manage-systemV6";
 
 
             
